46fea931aeeffb66fc6b721e1589290fa99a7f81
Rodrigo Emanuel Gomez Muñoz <emanuelmunoz695@gmail.com>
2025-11-11 12:41:30 -0300

    Actualización 11/11/25 12:42

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ window.addEventListener('DOMContentLoaded', ajustarHero);
 window.addEventListener('resize', ajustarHero);
 
 // FIN DEL AJUSTE AUTOMATICO DE LA NAV CON EL HERO //
+
+// FILTRO SECCIÓN GALERÍA //
+
+function filtrarPorClase(claseMostrar) {
+    const imagenes = document.querySelectorAll('.contenedorIMGSeccionGaleria');
+
+    imagenes.forEach(div => {
+        if (claseMostrar === 'todo') {
+            div.style.display = 'block';
+        }
+        else if (div.classList.contains(claseMostrar)) {
+            div.style.display = 'block';
+        } else {
+            div.style.display = 'none';
+        }
+    });
+}
+
+document.querySelector('.buttonTodo').addEventListener('click', () => {
+    filtrarPorClase('todo');
+});
+
+document.querySelector('.buttonDia').addEventListener('click', () => {
+    filtrarPorClase('filtroDia');
+});
+
+document.querySelector('.buttonNoche').addEventListener('click', () => {
+    filtrarPorClase('filtroNoche');
+});
+
+document.querySelector('.buttonEventos').addEventListener('click', () => {
+    filtrarPorClase('filtroEventos');
+});
+
+// FIN DEL FILTRO SECCIÓN GALERÍA //
